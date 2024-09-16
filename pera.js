42188f4169c3c8a98c9d11639f7838734a4ce24e
Ali Zaheer <soomrozaheer018@gmail.com>
2024-09-16 11:38:19 +0500

    css work

---
+++ pera.js
@@ -0,0 +1,131 @@
+
+
+//https://photos.google.com/share/AF1QipPjiorjr-dwmbElJ7YCZa8EYXgUPOBJUMoeHmS9hxRDWKqMyIbKpLybtc4IW9SkQQ/photo/AF1QipMBfTod9ziBz_fzbzXqVkkkmZo1Nokq16DUQPxY?key=WEFjZHI5b3BGZjlwbEhZVlhKUUhYSzdkWTdqYm53
+
+
+// for (let a = 1; a <= 10; a++){
+//     console.log("hello all of you");
+// }
+
+
+
+//0array
+
+// let array =["zaheer","saarang","arslan"];
+// console.log(array);
+// array.pop();  // remove for last value
+// console.log(array);
+// array.push("tariq");
+// console.log(array);
+// array.shift();
+// console.log(array);
+// array.unshift("israr");
+// console.log(array);
+// array.reverse();
+// console.log(array);
+// array.splice(2,0,"ali");
+// console.log(array);
+// array.slice();
+// console.log(array);
+
+
+//synchronous
+
+
+
+// alert ("pakistan");
+// console.log("hello dear")
+
+
+
+//Asynchronous
+
+// setTimeout(function(){
+//     console.log("hello pakistan")
+// })
+// console.log("pakistan hello");
+
+
+
+//object 
+// let y = {
+//     name : "Kaleemullah ",
+//     caste : " Kalwar",
+// }
+// console.log( y);
+
+
+// let x ={
+//     name : "zaheer ali soomro",
+//     country : "pakistan",
+// }
+// console.log(x);
+
+// prompt
+ 
+// let name = prompt("how are you");
+// console.log(name);
+
+// let randomnumber = Math.round();
+// console.log(randomnumber);
+
+
+// let a = 56;
+// let b = 98;
+// console.log(a + b);
+
+// function std(){
+//     let std =   {
+//         name : prompt("enter your name"),
+//     }
+
+//      std1.push(std);
+// let stringfly = JSON.stringify(std);
+// localStorage.setItem("stringfly,stringfly");
+// console.log(std1);
+// }
+
+//PROMISE 
+
+
+// function runtime(value) {
+//     return new Promise ((resolve) =>{   
+//         setTimeout(function(){
+//             let data = 50 + value
+//             resolve(data)
+//         },3000)
+//     }
+// )
+// }
+// runtime(50).then(function(data){
+//     console.log(data);
+// })
+
+//reject
+
+// function runtime(value) {
+//     return new Promise ((reject) =>{   
+//         setTimeout(function(){
+//             let data = 50 + value
+//             reject(data)
+//         },3000)
+//     }
+// )
+// }
+// runtime(50).catch(function(data){
+//     console.log(data);
+// })
+
+// let name = document.getElementById("name");
+// console.log(name);
+
+
+// alert(" Zaheer Khan Official");
+// document.write("hello world");
+
+
+// setTimeout(function (){
+//     document.write("this is DOM running ")
+// },5000)
+
+// document.write("this is document");
